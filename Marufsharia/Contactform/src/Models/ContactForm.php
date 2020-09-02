13a19e34eaedf7b5c25e470dce77b31090d50011
Maruf Sharia <marufsharia@gmail.com>
2020-09-02 10:23:28 +0600

    initial commit

---
+++ Marufsharia/Contactform/src/Models/ContactForm.php
@@ -0,0 +1,8 @@
+<?php
+    namespace Marufsharia\Contactform\Models;
+    use Illuminate\Database\Eloquent\Model;
+    class ContactForm extends Model
+    {
+        protected $guarded = [];
+        protected $table = 'contact';
+    }
